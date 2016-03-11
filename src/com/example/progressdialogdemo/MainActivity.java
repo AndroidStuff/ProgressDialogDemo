eb76954458ad4e6b1ef34537fdd80608a8299fec
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-03-11 13:48:49 +0530

    Implemented Progress Dialog feature :)

--- src/com/example/progressdialogdemo/MainActivity.java
+++ src/com/example/progressdialogdemo/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.progressdialogdemo;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	private ProgressDialog progressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void showProgressBar(View v) {
+		new RandomAsyncTask().execute();
+	}
+
+	private void showProgressDialog(CharSequence message) {
+		if (progressDialog == null) {
+			progressDialog = new ProgressDialog(this);
+			progressDialog.setIndeterminate(true);
+		}
+		progressDialog.setMessage(message);
+		progressDialog.show();
+	}
+
+	private void dismissProgressDialog() {
+		if (progressDialog != null) {
+			progressDialog.dismiss();
+		}
+	}
+
+
+	class RandomAsyncTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			showProgressDialog("Relax!..while we are fetching the data for you.");
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			dismissProgressDialog();
+		}
 	}
 }
